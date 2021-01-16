efb4e582b5ae6dbdec81950e8cee209cffe83382
simeonbs <simeonstoimenov@gmail.com>
2021-01-16 16:23:48 +0200

    Added Multidimensional arrays exercises from lab.

---
+++ Programming-Advanced/Multidimensional-Arrays-Lab/01.SumMatrixElements/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+
+            
+
+            int[,] matrix = new int[rows, cols];
+
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRow = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            long sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ Programming-Advanced/Multidimensional-Arrays-Lab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+
+
+
+            int[,] matrix = new int[rows, cols];
+
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRow = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+
+                }
+                Console.WriteLine(sum);
+
+            }
+        }
+    }
+}
