65b08172352ff3c88dd9717b781a252aac4e10e4
simeonbs <simeonstoimenov@gmail.com>
2021-02-05 21:00:29 +0200

    Added some advanced exam exercises.

---
+++ Programming-Advanced/AdvancedExamPrep/01Cooking/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> items = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Dictionary<string, int> madeItems = new Dictionary<string, int>
+            {
+                {"Bread", 0 },
+                {"Cake", 0 },
+                {"Fruit Pie", 0 },
+                {"Pastry", 0 }
+            };
+
+            while (liquids.Count > 0 && items.Count > 0)
+            {
+                int currLiquid = liquids.Dequeue();
+                int currItem = items.Pop();
+
+                int sum = currLiquid + currItem;
+
+                switch (sum)
+                {
+                    case 25:
+                        madeItems["Bread"]++;
+                       
+                        continue;
+                    case 50:
+                        madeItems["Cake"]++;
+                        
+                        continue;
+                    case 75:
+                        madeItems["Fruit Pie"]++;
+                        
+                        continue;
+                    case 100:
+                        madeItems["Pastry"]++;
+                        
+                        continue;
+                    default:
+                        items.Push(currItem + 3);
+                        break;
+                }
+
+                
+            }
+
+            if (!madeItems.ContainsValue(0))
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            if (madeItems.ContainsValue(0))
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquids.Count == 0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (liquids.Count > 0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+
+            if (items.Count == 0)
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+
+            if (items.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", items)}");
+            }
+
+            foreach (var item in madeItems)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ Programming-Advanced/AdvancedExamPrep/02.Selling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace _02.Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[n, n];
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            for (int r = 0; r < n; r++)
+            {
+                string rowData = Console.ReadLine();
+                char[] rowDataChar = rowData.ToCharArray();
+                for (int c = 0; c < n; c++)
+                {
+                    if (rowDataChar[c] == 'S')
+                    {
+                        playerCol = c;
+                        playerRow = r;
+
+                    }
+
+                    if (true)
+                    {
+
+                    }
+
+                    if (true)
+                    {
+
+                    }
+                    matrix[r, c] = rowDataChar[c].ToString();
+                }
+            }
+
+            ;
+        }
+    }
+}
