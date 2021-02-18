c45618124ec19aa6a8d0d34d0b0861e73b652c3a
simeonbs <simeonstoimenov@gmail.com>
2021-02-18 22:25:57 +0200

    Added defining classes lab.

---
+++ Programming-Advanced/Defining-Classes/CarConstructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
