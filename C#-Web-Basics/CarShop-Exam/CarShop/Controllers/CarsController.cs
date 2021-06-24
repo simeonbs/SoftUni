54556dd8702c6ce54d0d995b9c0f337982588dab
simeonbs <simeonstoimenov@gmail.com>
2021-06-24 14:57:17 +0300

    Added user functionality.

---
+++ C#-Web-Basics/CarShop-Exam/CarShop/Controllers/CarsController.cs
@@ -0,0 +1,16 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Controllers
+{
+    public class CarsController : Controller
+    {
+        public HttpResponse All() => View();
+
+    }
+}
